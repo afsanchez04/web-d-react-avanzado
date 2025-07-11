0de2c5c2ea9122d9939e2f8c34cf261243ee875d
afsaanchez04 <afsanchez04@gmail.com>
2025-07-10 20:24:59 -0500

    validación finalizada

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -21,7 +21,9 @@ export const BasicForm = () => {
     register,
     handleSubmit,
     formState: { errors, isValid }
-  } = useForm()
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   const onSubmit = (data) => {
     console.log(data)
@@ -29,10 +31,38 @@ export const BasicForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type='text' {...register('username')} placeholder='Usuario' />
-      <input type='password' {...register('password')} placeholder='Password' />
-      <input type='password' {...register('confirmPassword')} placeholder='Confirm password' />
-      <button type='submit'>Enviar</button>
+      <input
+        type='text'
+        {...register('username')}
+        placeholder='Usuario'
+        className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400'
+      />
+      {errors.username && <p>{errors.username.message}</p>}
+      <br />
+      <input
+        type='password'
+        {...register('password')}
+        placeholder='Password'
+        className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400'
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+      <br />
+      <input
+        type='password'
+        {...register('confirmPassword')}
+        placeholder='Confirm password'
+        className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400'
+      />
+      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+      <br />
+      <button
+        type='submit'
+        disabled={!isValid}
+        className={`w-full py-2 rounded transition cursor-pointer 
+              ${isValid ? 'bg-blue-600 text-white hover:bg-blue-700' : 'bg-gray-300 text-gray-500 cursor-not-allowed'}`}
+
+      >Enviar
+      </button>
     </form>
   )
 }
