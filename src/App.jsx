9c0f3790a9d6c45a7f35480a11221f15ebaf5367
afsaanchez04 <afsanchez04@gmail.com>
2025-07-15 21:09:47 -0500

    creación de custom hook useOllama

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,6 @@ const schema = yup.object({
     .required('El mensaje es obligatorio')
 })
 
-const initialState = {
-  messages: []
-}
-
-const chatReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_MESSAGE':
-      console.log('agregando mensaje...')
-      console.log(state)
-      return { ...state, messages: [...state.messages, action.payload] }
-    default:
-      return state
-  }
-}
-
 export const App = () => {
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(schema)
@@ -34,26 +19,10 @@ export const App = () => {
   // Guarda la respuesta de llama2
   const [response, setResponse] = useState('')
   const [loading, setLoading] = useState(false)
-  const [state, dispatch] = useReducer(chatReducer, initialState)
 
   const handlePregunta = async (data) => {
     console.log(data)
     setLoading(true)
-    try {
-      const res = await axios.post('http://localhost:11434/api/generate', {
-        model: 'deepseek-r1:1.5b',
-        prompt: data.userInput,
-        stream: false
-      })
-      setResponse(res.data.response)
-      // Dispatch para guardar el mensaje del usuario
-      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'user', text: data.userInput } })
-      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'bot', text: res.data.response } })
-    } catch (error) {
-      console.error('error: ', error)
-    } finally {
-      setLoading(false)
-    }
   }
 
   return (
